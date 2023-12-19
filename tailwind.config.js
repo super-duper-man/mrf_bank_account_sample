dd5c017b019faf693c6fc1f3c9a3c69123221dc2
unknown <m.r.fouladi7@gmail.com>
2023-12-19 14:45:23 +0330

    project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
